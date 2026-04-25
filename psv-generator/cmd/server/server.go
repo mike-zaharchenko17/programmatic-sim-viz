79a3c655caa697c61f42a10133391c1338a049fd
mike-zaharchenko17 <mz2433@nyu.edu>
2026-04-25 17:14:32 -0400

    dummy websocket server (mostly to get compiler to stop complaining about imports); installed gorilla websocket and echo

---
+++ psv-generator/cmd/server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func hello(c *echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+
+	if err != nil {
+		return nil
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error("failed to write WS message", "error", err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error("failed to read WS message", "error", err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.Static("/", "../public")
+
+	e.GET("/ws", hello)
+
+	sc := echo.StartConfig{Address: ":1323"}
+	if err := sc.Start(context.Background(), e); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
--- psv-generator/go.mod
+++ psv-generator/go.mod
@@ -3,3 +3,9 @@ module psv-generator
 go 1.25.0
 
 require github.com/google/uuid v1.6.0
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/labstack/echo/v5 v5.1.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+)
