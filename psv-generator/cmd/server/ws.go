51674ebd4aba23aacd22fea58511374b074b4070
mike-zaharchenko17 <mz2433@nyu.edu>
2026-05-02 14:31:32 -0400

    removed unused WsHandlerWithChannel

--- psv-generator/cmd/server/ws.go
+++ psv-generator/cmd/server/ws.go
@@ -17,28 +17,6 @@ var (
 	}
 )
 
-func WsHandlerWithChannel(auctionResultChan chan generator.AuctionResult) echo.HandlerFunc {
-	return func(c *echo.Context) error {
-		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-
-		if err != nil {
-			return nil
-		}
-
-		defer ws.Close()
-
-		for auctionRes := range auctionResultChan {
-			data, _ := json.Marshal(auctionRes)
-
-			if err := ws.WriteMessage(websocket.TextMessage, data); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}
-}
-
 func WsHandlerWithHub(hub *BroadcastHub) echo.HandlerFunc {
 	return func(c *echo.Context) error {
 		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
