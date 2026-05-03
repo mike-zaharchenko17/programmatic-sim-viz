f1677697b0e05c28292fcb4605846fffebc5672b
mike-zaharchenko17 <mz2433@nyu.edu>
2026-05-03 18:44:11 -0400

    metric derivations from visibleResults

--- psv-svelte/src/lib/components/exploratory/KPIDefault.svelte
+++ psv-svelte/src/lib/components/exploratory/KPIDefault.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
     let { visibleResults } = $props()
+    import type { AuctionResult } from "$lib/types/types";
 
     let numAuctions = $derived(visibleResults.length)
+    let numWinners = $derived(visibleResults.filter((res: AuctionResult) => res.winner !== undefined).length)
+    let numLosers = $derived(visibleResults.reduce((acc: number, res: AuctionResult) => res.losers ? acc + res.losers.length : acc += 0, 0))
 </script>
 
 <div>
     <p>Num Auctions: {numAuctions}</p>
+    <p>Num Winners: {numWinners}</p>
+    <p>Num Losers: {numLosers}</p>
 </div>
